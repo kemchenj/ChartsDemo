ea628f8ce0f65925cd1e6b3cd6ad222845db9605
Kem Chen <chenkem@gmail.com>
2019-04-28 22:23:33 +0800

    initialize

---
+++ Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by kemchenj on 2019/4/28.
+//  Copyright © 2019 kemchenj. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
+class SomeCombiendChartView: CombinedChartView {
+
+    override func initialize() {
+        super.initialize()
+    }
+}
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '12.0'
+
+target 'Demo' do
+  use_frameworks!
+
+  pod 'Charts'
+
+end
